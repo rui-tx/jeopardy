fd18c458b21c0c897f5bb430be55025b30b459bb
Rui Teixeira <ruiteixeira@minderacodeacademy.com>
2024-06-27 11:01:13 +0100

    test

---
+++ src/com/mindera/mindswap/server/Messages.java
@@ -0,0 +1,17 @@
+package com.mindera.mindswap.server;
+
+public enum Messages {
+    OK,
+    ERROR,
+    PLAYER_JOINED,
+    PLAYER_LEFT,
+    GAME_STARTED,
+    GAME_ENDED,
+    GAME_RESULTS,
+    GAME_QUESTION,
+    GAME_ANSWER,
+    GAME_ANSWER_INVALID,
+    GAME_ANSWER_TIMEOUT,
+    GAME_ANSWER_CORRECT,
+    GAME_ANSWER_INCORRECT
+}
--- src/com/mindera/mindswap/server/Server.java
+++ src/com/mindera/mindswap/server/Server.java
@@ -1,4 +1,94 @@
 package com.mindera.mindswap.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Server {
+
+    private final String PRETTY_NAME = "Jeopardy Server";
+    private final String HOST = "localhost";
+    private final int PORT = 15000;
+
+    private final List<ClientHandler> connectionList = new LinkedList<>();
+    private final ExecutorService threadPool = Executors.newCachedThreadPool();
+    private ServerSocket socket;
+
+
+    public Server() {
+        //test
+        //this is a test
+    }
+
+    public void start() {
+
+        ServerSocket server = this.init();
+
+        while (true) {
+            Socket clientSocket;
+            synchronized (server) {
+                clientSocket = this.initClientConnection(server); // blocking method
+            }
+
+            ClientHandler conn = new ClientHandler(this, clientSocket);
+            Thread thread = new Thread(conn, clientSocket.getPort() + "");
+
+            threadPool.submit(thread);
+            connectionList.add(conn);
+            this.sendMessageToClients("[server]> [" + clientSocket.getPort() + "] joined the server");
+        }
+
+    }
+
+    private ServerSocket init() {
+
+        try {
+            this.socket = new ServerSocket(PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.printf("[server]> socket created at port %d\n", PORT);
+        return this.socket;
+    }
+
+    private Socket initClientConnection(ServerSocket serverSocket) {
+        Socket socket;
+
+        try {
+            socket = serverSocket.accept();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+
+        return socket;
+    }
+
+
+    public static class ClientHandler implements Runnable {
+
+        private final Server server;
+        private Socket socket;
+        private String clientName;
+        private BufferedReader in;
+        private PrintWriter out;
+
+        public ClientHandler(Server server, Socket socket) {
+            this.server = server;
+            this.socket = socket;
+        }
+
+
+        @Override
+        public void run() {
+
+        }
+    }
 }
