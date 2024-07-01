09a17de956f55757f2f434f0a25cbaf3079e1990
Rui Teixeira <ruiteixeira@minderacodeacademy.com>
2024-07-01 11:10:05 +0100

    Refactor some code, added Messages enum, added javadoc to functions

---
+++ src/com/mindera/mindswap/Messages.java
@@ -0,0 +1,46 @@
+package com.mindera.mindswap;
+
+public enum Messages {
+
+    //Client messages
+    CLIENT_CONNECTED("Client connected"),
+    CLIENT_DISCONNECTED("Client disconnected"),
+    CLIENT_MESSAGE("Client message"),
+    LOCKED_OUT("You are locked out!"),
+    UNLOCKED("You are now unlocked."),
+
+    //Server messages
+    SERVER_CONNECTED("Server connected"),
+    SERVER_DISCONNECTED("Server disconnected"),
+    SERVER_OFFLINE("Server offline"),
+    SERVER_MESSAGE("Server message"),
+
+    //Connection messages
+    CONNECTION_ACCEPTED("Connection accepted"),
+    CONNECTION_REFUSED("Connection refused"),
+    CONNECTION_CLOSED("Connection closed"),
+    CONNECTION_LOST("Connection lost"),
+
+    //Error messages
+    ERROR("Error"),
+    ;
+
+    private final String message;
+
+    Messages(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+
+    public static void printMessage(Messages message) {
+        System.out.println(message.toString());
+    }
+
+    public static void printMessage(Messages message, String description) {
+        System.out.println(message.toString() + ": " + description);
+    }
+}
--- src/com/mindera/mindswap/client/Client.java
+++ src/com/mindera/mindswap/client/Client.java
@@ -1,98 +1,173 @@
 package com.mindera.mindswap.client;
 
+import com.mindera.mindswap.Messages;
+
 import java.io.*;
 import java.net.Socket;
 
 public class Client {
 
-    private boolean locked = false;
+    private Socket socket;
+    private boolean locked;
     private long startTime;
     private long stopTime;
     private long messageTime;
 
+    /**
+     * Main method to start the client application.
+     *
+     * @param args Command-line arguments (not used).
+     */
     public static void main(String[] args) {
         Client client = new Client();
         try {
             client.start("localhost", 15000);
         } catch (IOException e) {
-            System.out.println("Connection closed...");
+            Messages.printMessage(Messages.SERVER_OFFLINE);
+            return;
         }
-
     }
 
+    /**
+     * Starts the client and connects to the specified server.
+     *
+     * @param host The server host name.
+     * @param port The server port number.
+     * @throws IOException If an I/O error occurs when creating the streams from the socket or when reading from the input stream.
+     */
     private void start(String host, int port) throws IOException {
         Socket socket = new Socket(host, port);
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        locked = false;
 
         new Thread(new KeyboardHandler(out, socket)).start();
-        String line;
-        while ((line = in.readLine()) != null) {
+        String receivedMessage;
+        while ((receivedMessage = in.readLine()) != null) {
 
-            if (line.equals("/lock")) {
+            if (receivedMessage.equals("/lock")) {
                 locked = true;
-                System.out.println("You are locked out!");
+                Messages.printMessage(Messages.LOCKED_OUT);
                 continue;
             }
 
-            if (line.equals("/unlock")) {
+            if (receivedMessage.equals("/unlock")) {
                 locked = false;
-                System.out.println("You are now unlocked.");
+                Messages.printMessage(Messages.UNLOCKED);
                 startTime = System.currentTimeMillis();
                 continue;
             }
 
-            System.out.println(line);
+            System.out.println(receivedMessage);
         }
-        socket.close();
+
+        closeConnection();
+        quit();
     }
 
+    /**
+     * Closes the connection to the server.
+     */
+    private void closeConnection() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            Messages.printMessage(Messages.CONNECTION_CLOSED, "but an error occurred: " + e.getMessage());
+            quit();
+        } catch (NullPointerException e) {
+            Messages.printMessage(Messages.CONNECTION_LOST, "Socket closed / Lost connection to server");
+            quit();
+        }
+    }
+
+    /**
+     * Quit application.
+     */
+    private void quit() {
+        System.exit(0);
+    }
+
+    /**
+     * Handles keyboard input from the user and sends it to the server.
+     */
     private class KeyboardHandler implements Runnable {
         private final BufferedWriter out;
         private final Socket socket;
         private final BufferedReader in;
 
+        /**
+         * Constructs a new KeyboardHandler.
+         *
+         * @param out The BufferedWriter to send messages to the server.
+         * @param socket The client socket connected to the server.
+         */
         public KeyboardHandler(BufferedWriter out, Socket socket) {
             this.out = out;
             this.socket = socket;
             this.in = new BufferedReader(new InputStreamReader(System.in));
         }
 
+        /**
+         * Runs the keyboard handler thread, reading input from the user and sending it to the server.
+         */
         @Override
         public void run() {
 
             while (!socket.isClosed()) {
                 try {
-                    String line = in.readLine();
+                    String input = in.readLine();
 
                     if (locked) {
-                        System.out.println("You are locked out!");
+                        Messages.printMessage(Messages.LOCKED_OUT);
                         continue;
                     }
 
                     stopTime = System.currentTimeMillis();
                     messageTime = stopTime - startTime;
-                    line = line + ";" + messageTime;
 
-                    out.write(line);
-                    out.newLine();
-                    out.flush();
+                    send(input);
 
-
-                    if (line.equals("/quit")) {
-                        socket.close();
-                        System.exit(0);
+                    if (input.equals("/quit")) {
+                        closeConnection();
+                        quit();
                     }
 
                 } catch (IOException e) {
-                    System.out.println("Server connection lost...");
-                    try {
-                        socket.close();
-                    } catch (IOException ex) {
-                        ex.printStackTrace();
-                    }
+                    Messages.printMessage(Messages.CONNECTION_LOST);
+                    closeConnection();
                 }
             }
+        }
+
+        /**
+         * Sends a message to the server after encoding it.
+         *
+         * @param message The message to send.
+         */
+        private void send(String message) {
+            try {
+                synchronized (out) {
+                    out.write(encodeMessage(message));
+                    out.newLine();
+                    out.flush();
+                }
+
+            } catch (IOException e) {
+                Messages.printMessage(Messages.CONNECTION_LOST, "Error sending message to server");
+                closeConnection();
+                quit();
+            }
+        }
+
+        /**
+         * Encodes the message by appending the message time.
+         * In the future, other type of values maybe appended to the message.
+         *
+         * @param message The message to encode.
+         * @return The encoded message with the message time appended.
+         */
+        private String encodeMessage(String message) {
+            return message + ";" + messageTime;
         }
     }
 }
