e588e3e3b80863d4457930d787e09f714d338513
Rui Teixeira <ruiteixeira@minderacodeacademy.com>
2024-07-04 22:30:32 +0100

    Javadoc added to utils classes.

--- src/com/mindera/mindswap/utils/CSVReader.java
+++ src/com/mindera/mindswap/utils/CSVReader.java
@@ -8,11 +8,23 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
-
+/**
+ * Reads a CSV file and returns a map of items by key.
+ */
 public class CSVReader {
 
-    private CSVReader() {}
+    private CSVReader() {
+    }
 
+    /**
+     * Reads a CSV file and returns a map of items by key.
+     *
+     * @param csvFile  the CSV file to read
+     * @param mapper   a function that maps a string array to an item
+     * @param keyIndex the index of the key in the string array
+     * @param <T>      the type of the item
+     * @return a map of items by key
+     */
     public static <T> Map<String, ArrayList<T>> readItems(String csvFile, Function<String[], T> mapper, int keyIndex) {
         Map<String, ArrayList<T>> itemsByKey = new HashMap<>();
         String line;
